8972ab4127b2cf1904b1549048b7d620b7ab9dec
Om <oms87473@gmail.com>
2023-02-19 15:44:26 +0530

    original version

--- src/components/pages/home/highlights/item/item.jsx
+++ src/components/pages/home/highlights/item/item.jsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import { Popover } from '@headlessui/react';
+import { Popover, Transition } from '@headlessui/react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+import React, { Fragment } from 'react';
+
 import Heading from 'components/shared/heading';
-import ChevronRightIcon from 'icons/chevronright.inline.svg';
-import Tooltip from './Tooltip/Tooltip';
+import ChevronIcon from 'icons/chevrondown.inline.svg';
 
 const Item = ({ icon: Icon, title, description }) => (
-  <Popover as="li" className="relative last:mb-6">
-    <>
-      <Tooltip content={description} direction={'right'} delay={100}>
-        <Popover.Button className=" flex h-full w-full items-center rounded-lg border border-gray-3 p-1 transition duration-200 hover:bg-sky-700 xl:p-1 xl:pl-6">
+  <Popover as="li" className="relative">
+    {({ open }) => (
+      <>
+        <Popover.Button
+          className={classNames(
+            'flex h-full w-full items-center rounded-lg border border-gray-3 p-4 transition duration-200 xl:p-5 xl:pl-6',
+            open && 'rounded-b-none border-transparent shadow-primary'
+          )}
+        >
           <Icon className="h-auto w-16 shrink-0 xl:w-max" />
           <Heading className="mx-4 text-left" size="3xs" tag="h3">
             {title}
           </Heading>
-          <ChevronRightIcon className="ml-auto h-4 w-auto shrink-0 pr-1 " />
+          <ChevronIcon className="ml-auto h-4 w-auto shrink-0 rotate-90" />
         </Popover.Button>
-      </Tooltip>
-    </>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-200"
+          enterFrom="opacity-0 translate-y-1"
+          enterTo="opacity-100 translate-y-0"
+          leave="transition ease-in duration-150"
+          leaveFrom="opacity-100 translate-y-0"
+          leaveTo="opacity-0 translate-y-1"
+        >
+          <Popover.Panel className="absolute left-1/2 top-[98%] z-10 w-full -translate-x-1/2 rounded-b-lg bg-white px-6 text-left shadow-secondary">
+            <div
+              className="border-t border-gray-3 py-6"
+              dangerouslySetInnerHTML={{ __html: description }}
+            />
+          </Popover.Panel>
+        </Transition>
+      </>
+    )}
   </Popover>
 );
 
+Item.propTypes = {
+  icon: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
 export default Item;
-//
